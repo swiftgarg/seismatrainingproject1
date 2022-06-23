9d44ce2d84bec96c03a1322f8a5ac6cd7beb7b86
Kshitij Garg <kshitij.2054@gmail.com>
2022-06-23 13:54:18 +1000

    Added Lambda : Still working on them

---
+++ src/main/java/com/example/seismatraining1/FileUploadHandler.java
@@ -0,0 +1,28 @@
+package com.example.seismatraining1;
+
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.RequestHandler;
+import com.amazonaws.services.lambda.runtime.events.SQSEvent;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.io.File;
+
+public class FileUploadHandler implements RequestHandler<SQSEvent, String> {
+
+    private AmazonClientS3Service clientS3Service;
+
+
+    @Autowired
+    FileUploadHandler(AmazonClientS3Service amazonClient) {
+        this.clientS3Service = amazonClient;
+
+    }
+
+    @Override
+    public String handleRequest(SQSEvent input, Context context) {
+
+        clientS3Service.uploadFileTos3bucket("convertedFileFromCsv.json",new File("csv_testfiles/convertedFile.json"));
+
+        return "File Successfully Uploaded to S3";
+    }
+}
